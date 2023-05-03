7f625d47a83c3e56fdf9fa9550f0622c0bbe6790
07shrutik <shrutikmahajan1414@gmail.com>
2023-05-03 16:09:24 +0530

    Header Ui designed

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,7 +1,55 @@
+import DescriptionIcon from "@mui/icons-material/Description";
+import CommentIcon from "@mui/icons-material/Comment";
+import VideocamIcon from "@mui/icons-material/Videocam";
+import PersonAddAltIcon from "@mui/icons-material/PersonAddAlt";
+import DriveFileMoveIcon from "@mui/icons-material/DriveFileMove";
+import StarOutlineIcon from "@mui/icons-material/StarOutline";
+import styles from "./Head.module.css";
+import { Avatar } from "@mui/material";
+
 const Head = () => {
+  const options = [
+    "File",
+    "Edit",
+    "View",
+    "Insert",
+    "Format",
+    "Tools",
+    "Extension",
+    "Help",
+    "...",
+  ];
   return (
-    <div>
-      <h1>hello</h1>
+    <div className={styles.header}>
+      <div className={styles.left}>
+        <div className={styles.logo}>
+          <DescriptionIcon
+            sx={{ color: "#4285F4", height: "65px", width: "65px" }}
+          />
+        </div>
+        <div className={styles.menu}>
+          <div className={styles.heading}>
+            <p>Untitled document</p>
+            <div className={styles.headIcon}>
+              <StarOutlineIcon />
+              <DriveFileMoveIcon />
+            </div>
+          </div>
+          <div className={styles.options}>
+            {options.map((option, index) => (
+              <li key={index} style={{ listStyle: "none" }}>
+                {option}
+              </li>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className={styles.icons}>
+        <CommentIcon />
+        <VideocamIcon />
+        <PersonAddAltIcon />
+        <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+      </div>
     </div>
   );
 };
