8f653d84b2e4a02474c7c61c910437fbb703d195
Neelumishra <neelumishra0397@gmail.com>
2023-05-03 21:45:59 +0530

    DropDown Functionality

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -4,20 +4,152 @@ import VideocamIcon from "@mui/icons-material/Videocam";
 import PersonAddAltIcon from "@mui/icons-material/PersonAddAlt";
 import DriveFileMoveIcon from "@mui/icons-material/DriveFileMove";
 import StarOutlineIcon from "@mui/icons-material/StarOutline";
+import FeedIcon from "@mui/icons-material/Feed";
+import FolderOpenIcon from "@mui/icons-material/FolderOpen";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import ShareIcon from "@mui/icons-material/Share";
+import EmailIcon from "@mui/icons-material/Email";
+import DownloadIcon from "@mui/icons-material/Download";
+import DriveFileRenameOutlineIcon from "@mui/icons-material/DriveFileRenameOutline";
+import PrintIcon from "@mui/icons-material/Print";
+import AddBoxIcon from "@mui/icons-material/AddBox";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import RestoreIcon from "@mui/icons-material/Restore";
+import ReplayIcon from "@mui/icons-material/Replay";
+import RedoIcon from "@mui/icons-material/Redo";
+import ContentCutIcon from "@mui/icons-material/ContentCut";
+import SelectAllIcon from "@mui/icons-material/SelectAll";
+import ContentPasteIcon from "@mui/icons-material/ContentPaste";
+import ContentPasteOffIcon from "@mui/icons-material/ContentPasteOff";
+import FindReplaceIcon from "@mui/icons-material/FindReplace";
+import FullscreenIcon from "@mui/icons-material/Fullscreen";
+import CreateIcon from "@mui/icons-material/Create";
+import CheckIcon from "@mui/icons-material/Check";
 import styles from "./Head.module.css";
 import { Avatar } from "@mui/material";
 
 const Head = () => {
   const options = [
-    "File",
-    "Edit",
-    "View",
-    "Insert",
-    "Format",
-    "Tools",
-    "Extension",
-    "Help",
-    "...",
+    {
+      label: "File",
+      values: [
+        { name: "New", Icon: <FeedIcon /> },
+        { name: "Open", Icon: <FolderOpenIcon /> },
+        { name: "Make a Copy", Icon: <ContentCopyIcon /> },
+        { name: "Share", Icon: <ShareIcon /> },
+        { name: "Email", Icon: <EmailIcon /> },
+        { name: "Download", Icon: <DownloadIcon /> },
+        { name: "Rename", Icon: <DriveFileRenameOutlineIcon /> },
+        { name: "Move", Icon: <DriveFileMoveIcon /> },
+        { name: "Add a shortcut to Drive", Icon: <AddBoxIcon /> },
+        { name: "Language", Icon: <DeleteOutlineIcon /> },
+        { name: "History", Icon: <RestoreIcon /> },
+        { name: "Print", Icon: <PrintIcon /> },
+      ],
+    },
+
+    {
+      label: "Edit",
+      values: [
+        { name: "Undo", Icon: <ReplayIcon /> },
+        { name: "Redo", Icon: <RedoIcon /> },
+        { name: "Cut", Icon: <ContentCutIcon /> },
+        { name: "Copy", Icon: <ContentCopyIcon /> },
+        { name: "Paste", Icon: <ContentPasteIcon /> },
+        { name: "Paste without formatting", Icon: <ContentPasteOffIcon /> },
+        { name: "select all", Icon: <SelectAllIcon /> },
+        { name: "Delete", Icon: <DeleteOutlineIcon /> },
+        { name: "Find and replace", Icon: <FindReplaceIcon /> },
+      ],
+    },
+    {
+      label: "View",
+      values: [
+        { name: "Mode", Icon: <CreateIcon /> },
+        { name: "Show PrintOutLayout", Icon: <CheckIcon /> },
+        { name: "Show Ruler", Icon: <CheckIcon /> },
+        { name: "Show Outline", Icon: <CheckIcon /> },
+        { name: "Show equation toolbar", Icon: <CheckIcon /> },
+        { name: "Show non-print", Icon: <CheckIcon /> },
+        { name: "FullScreen", Icon: <FullscreenIcon /> },
+      ],
+    },
+    {
+      label: "Insert",
+      values: [
+        { name: "Image", Icon: <FullscreenIcon /> },
+        { name: "Table", Icon: <FindReplaceIcon /> },
+        { name: "Drawing", Icon: <RedoIcon /> },
+        { name: "Horizonatal Line ", Icon: <DeleteOutlineIcon /> },
+        { name: "Emoji", Icon: <ContentCopyIcon /> },
+        { name: "Smart chips", Icon: <SelectAllIcon /> },
+        { name: "Drop-down", Icon: <FindReplaceIcon /> },
+        { name: "footnote", Icon: <FindReplaceIcon /> },
+        { name: "Building blocks", Icon: <SelectAllIcon /> },
+        { name: "Special characters", Icon: <FindReplaceIcon /> },
+        { name: "Equation", Icon: <ReplayIcon /> },
+        { name: "Watermark", Icon: <FindReplaceIcon /> },
+        { name: "Headers and footers", Icon: <SelectAllIcon /> },
+        { name: "Page numbers", Icon: <FindReplaceIcon /> },
+        { name: "Break and Link", Icon: <SelectAllIcon /> },
+      ],
+    },
+    {
+      label: "Format",
+      values: [
+        { name: "Text", Icon: <FindReplaceIcon /> },
+        { name: "Paragraph Styles", Icon: <FindReplaceIcon /> },
+        { name: "Align and Indent", Icon: <ReplayIcon /> },
+        { name: "Line & paragraph spacing", Icon: <SelectAllIcon /> },
+        { name: "Colomns", Icon: <FindReplaceIcon /> },
+        { name: "Bullets and numbering", Icon: <SelectAllIcon /> },
+        { name: "Headers and Footers", Icon: <FindReplaceIcon /> },
+        { name: "Page numbers", Icon: <ReplayIcon /> },
+        { name: "Page orientaton", Icon: <SelectAllIcon /> },
+        { name: "Table", Icon: <FindReplaceIcon /> },
+        { name: "Image", Icon: <ReplayIcon /> },
+        { name: "Borders and lines", Icon: <SelectAllIcon /> },
+        { name: "Clear formatting", Icon: <SelectAllIcon /> },
+      ],
+    },
+    {
+      label: "Tools",
+      values: [
+        { name: "Spelling and Grammar", Icon: <FindReplaceIcon /> },
+        { name: "Word count", Icon: <CreateIcon /> },
+        { name: "Review suggested edits", Icon: <FindReplaceIcon /> },
+        { name: "Compare documents", Icon: <FindReplaceIcon /> },
+        { name: "Citations", Icon: <ReplayIcon /> },
+        { name: "Explore", Icon: <FindReplaceIcon /> },
+        { name: "Linked objects", Icon: <FindReplaceIcon /> },
+        { name: "Dictionary", Icon: <FindReplaceIcon /> },
+        { name: "Translate document", Icon: <ReplayIcon /> },
+        { name: "Voice Typing", Icon: <FindReplaceIcon /> },
+        { name: "Notifications Settings", Icon: <FindReplaceIcon /> },
+        { name: "Preferences", Icon: <ReplayIcon /> },
+        { name: "Notifications Settings", Icon: <FindReplaceIcon /> },
+        { name: "Accessibility", Icon: <ReplayIcon /> },
+      ],
+    },
+    {
+      label: "Extension",
+      values: [
+        { name: "Add-ons", Icon: <DeleteOutlineIcon /> },
+        { name: "Apps Script", Icon: <FindReplaceIcon /> },
+      ],
+    },
+    {
+      label: "Help",
+      values: [
+        { name: "Help", Icon: <DeleteOutlineIcon /> },
+        { name: "Traning", Icon: <FindReplaceIcon /> },
+        { name: "Updates", Icon: <FindReplaceIcon /> },
+        { name: "Help Docs improve", Icon: <DeleteOutlineIcon /> },
+        { name: "Privacy Policy", Icon: <FindReplaceIcon /> },
+        { name: "Terms of Service", Icon: <FindReplaceIcon /> },
+        { name: "Keyboard shortcuts", Icon: <DeleteOutlineIcon /> },
+      ],
+    },
   ];
   return (
     <div className={styles.header}>
@@ -38,7 +170,17 @@ const Head = () => {
           <div className={styles.options}>
             {options.map((option, index) => (
               <li key={index} style={{ listStyle: "none" }}>
-                {option}
+                <div class={styles.dropdown}>
+                  <button class={styles.dropbtn}>{option.label}</button>
+                  <div class={styles.dropdowncontent}>
+                    {option.values.map((e) => (
+                      <a className={styles.Iconname} href="#">
+                        <span style={{marginRight:"4px"}}>{e.Icon}</span>
+                        {e.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
               </li>
             ))}
           </div>
@@ -48,7 +190,11 @@ const Head = () => {
         <CommentIcon />
         <VideocamIcon />
         <PersonAddAltIcon />
-        <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        <Avatar
+          sx={{ marginBottom: "3rem" }}
+          alt="Remy Sharp"
+          src="/static/images/avatar/1.jpg"
+        />
       </div>
     </div>
   );
